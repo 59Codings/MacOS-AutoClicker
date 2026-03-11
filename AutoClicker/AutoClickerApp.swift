b56bf8b940ca827aff8760ed8dcfb4d2f4a6f1d6
59Codings <59clanww@gmail.com>
2026-03-11 15:42:33 +0100

    Initial Commit

---
+++ AutoClicker/AutoClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoClickerApp.swift
+//  AutoClicker
+//
+//  Created by 59 on 11/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AutoClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoClickerTests/AutoClickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoClickerTests.swift
+//  AutoClickerTests
+//
+//  Created by 59 on 11/3/26.
+//
+
+import Testing
+@testable import AutoClicker
+
+struct AutoClickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
